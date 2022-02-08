2633af9fe3c410c62949bbb7736127f106a21f43
Alex Emelyanov <alex84_84_84@mail.ru>
2022-02-08 19:39:45 +0300

    run from dataset

--- src/sly_globals.py
+++ src/sly_globals.py
@@ -8,7 +8,8 @@ api: sly.Api = my_app.public_api
 TASK_ID = int(os.environ["TASK_ID"])
 TEAM_ID = int(os.environ['context.teamId'])
 WORKSPACE_ID = int(os.environ['context.workspaceId'])
-PROJECT_ID = int(os.environ['modal.state.slyProjectId'])
+PROJECT_ID = int(os.environ['context.slyProjectId'])
+DATASET_ID = int(os.environ['context.slyDatasetId'])
 
 sample_percent = int(os.environ['gui.state.samplePercent'])
 save_mode = os.environ['gui.state.dstProjectMode']
