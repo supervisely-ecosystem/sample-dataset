ebcbad097e9d4a135cf1008eafab316e7d028d87
Alex Emelyanov <alex84_84_84@mail.ru>
2022-02-10 18:18:05 +0300

    add progress

--- src/init_ui.py
+++ src/init_ui.py
@@ -1,4 +1,8 @@
 
+import supervisely as sly
+from functools import partial
+
+
 def init_context(data, team_id, workspace_id, project_id):
     data["teamId"] = team_id
     data["workspaceId"] = workspace_id
@@ -12,3 +16,45 @@ def init_options(data, state):
     state["dstProjectId"] = None
 
     data["processing"] = False
+
+
+def init_progress(data, state):
+    data["progressName1"] = None
+    data["currentProgressLabel1"] = 0
+    data["totalProgressLabel1"] = 0
+    data["currentProgress1"] = 0
+    data["totalProgress1"] = 0
+
+
+def _set_progress(index, api, task_id, message, current_label, total_label, current, total):
+    fields = [
+        {"field": f"data.progressName{index}", "payload": message},
+        {"field": f"data.currentProgressLabel{index}", "payload": current_label},
+        {"field": f"data.totalProgressLabel{index}", "payload": total_label},
+        {"field": f"data.currentProgress{index}", "payload": current},
+        {"field": f"data.totalProgress{index}", "payload": total},
+    ]
+    api.task.set_fields(task_id, fields)
+
+
+def _update_progress_ui(api, task_id, progress: sly.Progress, index):
+    _set_progress(index, api, task_id, progress.message, progress.current_label, progress.total_label, progress.current, progress.total)
+
+
+def update_progress(count, index, api: sly.Api, task_id, progress: sly.Progress):
+    count = min(count, progress.total - progress.current)
+    progress.iters_done(count)
+    if progress.need_report():
+        progress.report_progress()
+        _update_progress_ui(api, task_id, progress, index)
+
+
+def get_progress_cb(api, task_id, index, message, total, is_size=False, func=update_progress):
+    progress = sly.Progress(message, total, is_size=is_size)
+    progress_cb = partial(func, index=index, api=api, task_id=task_id, progress=progress)
+    progress_cb(0)
+    return progress_cb
+
+
+def reset_progress(api, task_id, index):
+    _set_progress(index, api, task_id, None, 0, 0, 0, 0)
--- src/main.py
+++ src/main.py
@@ -19,7 +19,9 @@ def sample_dataset(api: sly.Api, task_id, context, state, app_logger):
 
     api.project.merge_metas(g.PROJECT_ID, dst_project_id)
 
-    for scr_dataset in api.dataset.get_list(g.PROJECT_ID):
+    datasets = api.dataset.get_list(g.PROJECT_ID)
+    progress_items_cb = init_ui.get_progress_cb(api, task_id, 1, "Finished", len(datasets))
+    for scr_dataset in datasets:
 
         image_infos = api.image.get_list(scr_dataset.id)
         image_ids = [image_info.id for image_info in image_infos]
@@ -31,8 +33,14 @@ def sample_dataset(api: sly.Api, task_id, context, state, app_logger):
         for sample_batch in sly.batched(sample_ids):
             api.image.copy_batch(curr_dst_ds.id, sample_batch, with_annotations=True)
 
+        progress_items_cb(1)
 
-    g.my_app.stop()
+    init_ui.reset_progress(api, task_id, 1)
+
+    api.app.set_field(task_id, "data.processing", False)
+    api.task.set_output_project(task_id, dst_project_id, project_name)
+
+    #g.my_app.stop()
 
 
 def main():
@@ -47,6 +55,7 @@ def main():
 
     init_ui.init_context(data, g.TEAM_ID, g.WORKSPACE_ID, g.PROJECT_ID)
     init_ui.init_options(data, state)
+    init_ui.init_progress(data, state)
 
     g.my_app.compile_template(g.root_source_dir)
     g.my_app.run(data=data, state=state)
